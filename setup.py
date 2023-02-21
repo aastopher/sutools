ca6914637f5b7e9624d5604d6ccea72aedd1ceb0
aastopher <aaron.stopher@gmail.com>
2023-02-21 00:20:49 -0700

    alter setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,26 @@
-## NEEDS EDIT!!
+import setuptools
 
-# import setuptools
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
-# with open("README.md", "r", encoding="utf-8") as fh:
-#     long_description = fh.read()
-
-# setuptools.setup(
-#     name="swiftutils",
-#     version="0.0.1",
-#     author="Aaron Stopher",
-#     description="swiftly setup module specific cli and loggers that work together",
-#     long_description=long_description,
-#     long_description_content_type="text/markdown",
-#     url="https://github.com/aastopher/swiftutils",
-#     project_urls={
-#         "Bug Tracker": "https://github.com/aastopher/swiftutils/issues",
-#     },
-#     keywords=['logs', 'logger', 'logging', 'CLI', 'cli', 'utils', 'swift'],
-#     classifiers=[
-#         "Programming Language :: Python :: 3",
-#         "License :: OSI Approved :: MIT License",
-#         "Operating System :: Any",
-#     ],
-#     package_dir={"": "src"},
-#     packages=setuptools.find_packages(where="src"),
-#     python_requires=">=3.6",
-# )
+setuptools.setup(
+    name="sutools",
+    version="0.1",
+    author="Aaron Stopher",
+    description="su (Super User) tools; per module utilities, designed to be lightweight, easy to configure, and reduce boilerplate code.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aastopher/sutools",
+    project_urls={
+        "Bug Tracker": "https://github.com/aastopher/sutools/issues",
+    },
+    keywords=['logs', 'logger', 'logging', 'CLI', 'cli', 'utils'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Any",
+    ],
+    package_dir={"": "sutools"},
+    packages=setuptools.find_packages(where="sutools"),
+    python_requires=">=3.8",
+)
