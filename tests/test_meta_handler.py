bfd57a525450dab4f848e60298df3fc4d324c5e6
aastopher <aaron.stopher@gmail.com>
2023-02-24 12:15:12 -0700

    tests for meta_handler completed

--- tests/test_meta_handler.py
+++ tests/test_meta_handler.py
@@ -1,21 +1,44 @@
 from unittest.mock import patch
 from io import StringIO
-from sutools import meta_handler
+import logging
+from sutools import meta_handler, cli_handler, log_handler
 
-#### Methods
-
-# Test 4: this should test adding references of functions 
+# Test 1: this should test adding references of functions 
 # for to the cli to run (a dictionary of functions should be provided 
 # then the cli should be tested to use those functions)
 def test_add_func():
-    pass
 
-# Test 5: this should test the parse method 
-# this is called after functions are added to be called as a command
+    def func_test():
+        pass
+
+    store = meta_handler.Bucket()
+    store.add_func(func_test)
+    assert 'func_test' in store.funcs.keys()
+
+# Test 2: this should test the add_cli method 
+# this the store should contain a cli obj
 def test_add_cli():
-    pass
+    store = meta_handler.Bucket()
+    cli_obj = cli_handler.CLI('desc', False)
+    store.add_cli(cli_obj)
+    assert isinstance(store.cli, cli_handler.CLI)
 
-# Test 5: this should test the parse method 
+# Test 2: this should test the parse method 
 # this is called after functions are added to be called as a command
 def test_add_log():
-    pass
+    store = meta_handler.Bucket()
+
+    log_obj = log_handler.Logger(
+                'test_logger', 
+                ['logger1','logger2','logger3'], 
+                logging.INFO, 
+                None, None, None, None, None, None, 
+                False, 
+                logging.Formatter('%(asctime)s, %(msecs)d %(name)s %(levelname)s %(message)s', datefmt='%H:%M:%S'), 
+                logging.StreamHandler(), 
+                stream=True
+              )
+
+    store.add_log(log_obj)
+    assert isinstance(store.log, log_handler.Logger)
+
