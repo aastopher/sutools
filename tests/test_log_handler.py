5745142c5131804955d5fd2a44c893a76dc47c62
aastopher <aaron.stopher@gmail.com>
2023-02-24 19:04:18 -0700

    completed test 5 in log test

--- tests/test_log_handler.py
+++ tests/test_log_handler.py
@@ -1,6 +1,6 @@
 from unittest.mock import patch, mock_open, MagicMock, Mock
 from io import StringIO
-import logging, sys, os, atexit
+import logging, sys, datetime
 from sutools import log_handler
 from pathlib import Path
 
@@ -113,8 +113,43 @@ def test_filepath_filename(monkeypatch):
 # Test 5: this should test passing a formatter object for the file logger
 # the result should be the file logger outputs a 
 # corresponding log in the given format
-def test_filefmt():
-    pass
+def test_filefmt(monkeypatch):
+    expected_filename = "test_file.log"
+
+    with patch('builtins.open', mock_open()) as mock_file:
+
+        # mock the file_controller method
+        def mock_file_controller(self):
+            self.fhandler.setLevel(self.loglvl) # set the level of the file handler
+            self.fhandler.setFormatter(self.filefmt) # set the formatter for the file handler
+            for log in vars(self.loggers).keys():
+                logger = logging.getLogger(log)
+                logger.addHandler(self.fhandler) # add the file handler to the logger
+                logger.propagate = False # disable propagation of log messages to ancestor loggers
+            return
+
+        monkeypatch.setattr(log_handler.Logger, "file_controller", mock_file_controller)
+
+        log_obj = log_handler.Logger(
+            name='test_name',
+            loggers=['log'],
+            loglvl=logging.INFO,
+            filename=expected_filename,
+            filepath='/logs/test_name',
+            filefmt=logging.Formatter('%(asctime)s, %(name)s %(levelname)s %(message)s', datefmt='%H:%M:%S'),
+            fhandler=logging.FileHandler(f'/logs/test_name/{expected_filename}', 'w', encoding='locale'),
+            filecap=None,
+            filetimeout=None,
+            file=True,
+            streamfmt=None,
+            shandler=None,
+            stream=False)
+
+        with patch.object(log_obj.loggers.log.handlers[0].stream, 'write') as mock_write:
+            log_time = datetime.datetime.now().strftime("%H:%M:%S")
+            log_obj.loggers.log.info('Test log message')
+            expected_msg = f'log INFO Test log message\n'
+            assert expected_msg in mock_write.call_args_list[0][0][0]
 
 # Test 6: this should test passing a handler object for the file logger
 # (i.e. `logging.FileHandler(filepath, 'w')`)
